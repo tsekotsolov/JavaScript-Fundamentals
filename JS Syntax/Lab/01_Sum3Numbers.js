b88e97ccdd0c0a6f1ef9b755460e272e5900233f
tsekotsolov <tsekotsolov@gmail.com>
2017-12-16 18:05:02 +0200

    Initial commit

---
+++ JS Syntax/Lab/01_Sum3Numbers.js
@@ -0,0 +1,37 @@
+// 1.	Sum 3 Numbers
+
+function sumOfNumbers(a, b, c) {
+  console.log(a + b + c);
+}
+
+//2.	Sum And VAT
+
+function vatCalculator(x) {
+
+  let sum = 0;
+
+  x.forEach(element => {
+    sum += element;
+  });
+
+  console.log("sum = ", sum);
+  let vat = sum * 20 / 100;
+
+  console.log("VAT = ", vat);
+
+  console.log("total = ", sum + vat);
+
+}
+
+//3.	Letter Occurrences in String
+
+
+function blink() {
+  var div = document.getElementById('greeting');
+
+  if (div.style.visibility === 'hidden') {
+    div.style.visibility = 'visible';
+  } else {
+    div.style.visibility = 'hidden'
+  }
+}
