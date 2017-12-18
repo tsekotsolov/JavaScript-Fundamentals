a277dae6aba30df1688f498b2e562ff7c74f64aa
Tseko Tsolov <tsekotsolov@gmail.com>
2017-12-18 23:21:59 +0200

    two tasks solved

--- Operators And Logic Flow/operators.js
+++ Operators And Logic Flow/operators.js
@@ -1,2 +1,15 @@
 'use strict';
+//1.	Multiply Numbers
 
+function multiply(x, y) {
+    return x * y;
+}
+
+//2.	Boxes and Bottles
+
+function findRequiredBoxes(numberOfBottles, boxCapacity) {
+
+    let boxesNeeded = numberOfBottles / boxCapacity;
+
+    return Math.ceil(boxesNeeded)
+}
