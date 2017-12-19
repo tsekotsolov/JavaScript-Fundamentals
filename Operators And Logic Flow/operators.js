26bfc39a07b498aead2ccb2a63af9178aff92bb4
tsekotsolov <tsekotsolov@gmail.com>
2017-12-19 15:58:45 +0200

    up

--- Operators And Logic Flow/operators.js
+++ Operators And Logic Flow/operators.js
@@ -13,3 +13,37 @@ function findRequiredBoxes(numberOfBottles, boxCapacity) {
 
     return Math.ceil(boxesNeeded)
 }
+
+let result = findRequiredBoxes(15,6);
+console.log(result);
+
+
+//3. Leap Year
+
+function leapYearCheck(year) {
+
+    let firstCondition = false;
+    let secondCondition = false;
+
+
+    if (year % 4 === 0) {
+        firstCondition = true;
+
+        if (year % 100 === 0) {
+
+            if (year % 400 === 0) {
+                secondCondition = true;
+            }
+        } else {
+            secondCondition = true;
+        }
+
+    }
+
+    if (firstCondition && secondCondition) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
+
+}
