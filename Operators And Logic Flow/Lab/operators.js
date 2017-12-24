c33d23abfdcc8103e6b60d4109d1c9d6bc55e858
tsekotsolov <tsekotsolov@gmail.com>
2017-12-24 14:23:26 +0200

    up

--- Operators And Logic Flow/Lab/operators.js
+++ Operators And Logic Flow/Lab/operators.js
@@ -49,3 +49,11 @@ function leapYearCheck(year) {
 }
 
 // 4.	Circle Area
+function calcCircleArea(radius){
+
+    let area = Math.PI*Math.pow(radius,2);
+
+    console.log(area);
+    console.log( area.toFixed(2));
+}
+
