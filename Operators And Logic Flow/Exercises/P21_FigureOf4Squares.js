8067b66ec1ce29e172f11b27405ca1b9c103a63a
Tseko Tsolov <tsekotsolov@gmail.com>
2017-12-27 22:07:57 +0200

    Task 21 changed

--- Operators And Logic Flow/Exercises/P21_FigureOf4Squares.js
+++ Operators And Logic Flow/Exercises/P21_FigureOf4Squares.js
@@ -1,33 +1,33 @@
 function drawFigure(n) {
 
-    let line ="+" + '-'.repeat(n-2) + "+" + '-'.repeat(n-2)+ "+\n" ;
-    let midline = "|" + ' '.repeat(n-2) + "|" + ' '.repeat(n-2)+ "|\n";
+    let line = "+" + '-'.repeat(n - 2) + "+" + '-'.repeat(n - 2) + "+\n";
+    let midline = "|" + ' '.repeat(n - 2) + "|" + ' '.repeat(n - 2) + "|\n";
 
     let result = line;
 
-    if (n%2!=0) {
-        for (let i = 1; i <= n-2; i++) {
-            if (i!=Math.ceil((n-2)/2)) {
-                result+=midline
+    if (n % 2 != 0) {
+        for (let i = 1; i <= n - 2; i++) {
+            if (i != Math.ceil((n - 2) / 2)) {
+                result += midline
             } else {
-                result+=line;
+                result += line;
             }
-       
+
         }
     } else {
-        for (let i = 1; i <= n-1-2; i++) {
-            
-            if (i!=(n-2)/2) {
-                result+=midline
+        for (let i = 1; i <= n - 1 - 2; i++) {
+
+            if (i != (n - 2) / 2) {
+                result += midline
             } else {
-                result+=line;
+                result += line;
             }
         }
     }
 
-    if (n!=2) {
-        result+=line;
-    } 
-    
+    if (n != 2) {
+        result += line;
+    }
+
     return result
 }
