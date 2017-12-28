dc29f4b9869730f77359839cfbb1a8d2c6e4a022
Tseko Tsolov <tsekotsolov@gmail.com>
2017-12-28 10:17:12 +0200

    P19 solved

---
+++ Operators And Logic Flow/Exercises/P19_QuadraticEquation.js
@@ -0,0 +1,19 @@
+function qEquation(a, b, c) {
+
+    let d = b ** 2 - 4 * a * c;
+
+    if (d < 0) {
+        return "No";
+
+    } else if (d === 0) {
+
+        x = -b / (2 * a)
+        return x;
+    } else if (d > 0) {
+
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+        return `${Math.min(x1,x2)}\n${Math.max(x1,x2)}`
+    }
+}
