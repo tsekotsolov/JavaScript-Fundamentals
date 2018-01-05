e0a8b84506fd85e187f4772e69de8419d5688e1c
tsekotsolov <tsekotsolov@gmail.com>
2018-01-05 20:24:21 +0200

    Lab solved

--- Functions/Lab/P07_WordsUppercase.js
+++ Functions/Lab/P07_WordsUppercase.js
@@ -1,25 +1,22 @@
 function wordsUppercase(input) {
 
   const regex = /\w+/g;
-  const str = input;
+ 
   let result = '';
 
-  while ((m = regex.exec(str)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
+  while ((m = regex.exec(input)) !== null) {
+    
     if (m.index === regex.lastIndex) {
       regex.lastIndex++;
     }
 
-    // The result can be accessed through the `m`-variable.
     m.forEach((match) => {
       
 
       result += match.toUpperCase()+ ','+' ';
     });
   }
-
   return result.substr(0, result.length-2);
 
 }
 
-console.log(wordsUppercase('k3 4jkhjk'));    
