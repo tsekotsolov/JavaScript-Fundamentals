b15afe29234d517a55b41999ce08f07d23d61980
tsekotsolov <tsekotsolov@gmail.com>
2018-01-05 20:24:56 +0200

    Lab solved

--- Functions/Lab/P07_WordsUppercase.js
+++ Functions/Lab/P07_WordsUppercase.js
@@ -1,22 +1,21 @@
 function wordsUppercase(input) {
 
   const regex = /\w+/g;
- 
+
   let result = '';
 
   while ((m = regex.exec(input)) !== null) {
-    
+
     if (m.index === regex.lastIndex) {
       regex.lastIndex++;
     }
 
     m.forEach((match) => {
-      
 
-      result += match.toUpperCase()+ ','+' ';
+
+      result += match.toUpperCase() + ',' + ' ';
     });
   }
-  return result.substr(0, result.length-2);
+  return result.substr(0, result.length - 2);
 
 }
-
