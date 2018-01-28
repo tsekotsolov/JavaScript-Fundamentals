f886d436f33a8822acedaa35436803e9e4172bc6
Tseko Tsolov <tsekotsolov@gmail.com>
2018-01-28 11:56:57 +0200

    up

---
+++ Objects&JSON/Exercises/P01_HeroicInventory.js
@@ -0,0 +1,30 @@
+function heroicInventory(input = []) {
+
+    let result = [];
+
+    for (let i = 0; i < input.length; i++) {
+        let [name, level, items] = input[i].split(' / ');
+
+
+        let itemList = [];
+
+        if (items!=undefined) {
+            itemList = items.split(', ');
+        }
+
+        let heroBasket = {
+            name: name,
+            level: Number(level),
+            items: itemList
+        }
+        result.push(heroBasket);
+    }
+    console.log(JSON.stringify(result));
+}
+
+heroicInventory(
+    [
+        'Isacc / 25 / ',
+        'Derek / 12 / BarrelVest, DestructionSword',
+        'Hes / 1 / Desolator, Sentinel, Antara',
+    ]);
