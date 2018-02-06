41f994e03f4d88edd6911da86e406655ce90f115
tsekotsolov <tsekotsolov@gmail.com>
2018-02-06 21:55:38 +0200

    up

--- 07.Exams/Exam 18.06.2017/P02_JansNotation/jan.js
+++ 07.Exams/Exam 18.06.2017/P02_JansNotation/jan.js
@@ -1,10 +1,51 @@
 function jan(input = []) {
 
+  let numbers = [];
+
+  for (const piece of input) {
+    if (!isNaN(piece)) {
+      numbers.push(piece);
+    } else {
+
+      if (numbers.length > 1) {
+
+        let secondNumber = numbers.pop();
+        let firstNumber = numbers.pop();
+
+        switch (piece) {
+          case '+':
+            numbers.push(firstNumber + secondNumber);
+            break;
+          case '-':
+            numbers.push(firstNumber - secondNumber);
+            break;
+          case '*':
+            numbers.push(firstNumber * secondNumber);
+            break;
+          case '/':
+            numbers.push(firstNumber / secondNumber);
+            break;
+          default:
+            break;
+        }
+      } else {
+        console.log('Error: not enough operands!');
+        return;
+      }
+    }
+  }
+
+  if (numbers.length > 1) {
+    console.log('Error: too many operands!');
+    return;
+  }
+  console.log(numbers.join(''));
+
 }
 
 jan([3, 4, '+']);
-jan([5, 3,4,'*', '-']);
-jan([7,33,8, '-']);
+jan([5, 3, 4, '*', '-']);
+jan([7, 33, 8, '-']);
 jan([15, '/']);
-jan([31,2,'+',11,'/']);
-jan([-1,1,'+',101,'*',18,'+',3,'/']);
+jan([31, 2, '+', 11, '/']);
+jan([-1, 1, '+', 101, '*', 18, '+', 3, '/']);
