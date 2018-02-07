2269bc5c0c65ae8b3557fcf6662fc2b5afeff6ee
tsekotsolov <tsekotsolov@gmail.com>
2018-02-07 17:22:28 +0200

    up

---
+++ 07.Exams/Exam16.12.2016/P02_BuildAWall/build.js
@@ -0,0 +1,34 @@
+function build(input = []){
+
+  input = input.map(e=>Number(e));
+  let concrete=[];
+
+  while (true) {
+
+    let dailyUsedConcrete=0;
+    for (let i = 0; i < input.length; i++) {
+      
+      if (input[i]<30) {
+        input[i]++;
+        dailyUsedConcrete+=195;
+      }
+    }
+
+    if (dailyUsedConcrete===0) {
+      break;
+    }
+    concrete.push(dailyUsedConcrete);
+    
+  }
+
+console.log(concrete.join(', '));
+
+console.log(`${(concrete.reduce((a,b)=>a+b))*1900} pesos`);
+
+}
+
+//build(["21", "25", "28"]);
+
+//build([17]);
+
+//build([17, 22, 17, 19, 17])
