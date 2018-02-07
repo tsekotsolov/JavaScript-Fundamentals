f6c39a5b9f966893ea7ade698aa3f0b08d0c95c4
tsekotsolov <tsekotsolov@gmail.com>
2018-02-07 07:55:43 +0200

    up

--- 07.Exams/Exam 18.06.2017/P01_Pyramid/pyramid.js
+++ 07.Exams/Exam 18.06.2017/P01_Pyramid/pyramid.js
@@ -1,8 +1,33 @@
-function pyramid(base,increment){
+function pyramid(base, increment) {
 
+  let [stone, marble, lapis, gold] = [0, 0, 0, 0];
+
+  let steps = Math.ceil(base / 2);
+
+  for (let i = 1; i <= steps - 1; i++) {
+
+    if (i % 5 != 0) {
+      currentStone = (base - 2) ** 2;
+      stone += currentStone*increment;
+      marble += (base ** 2 - currentStone)*increment;
+    } else {
+      currentStone = (base - 2) ** 2;
+      stone += currentStone*increment;
+      lapis += (base ** 2 - currentStone)*increment;
+    }
+    base -= 2;
+  }
+
+  gold=base**2*increment;
+
+  console.log(`Stone required: ${Math.ceil(stone)}`);
+  console.log(`Marble required: ${Math.ceil(marble)}`);
+  console.log(`Lapis Lazuli required: ${Math.ceil(lapis)}`);
+  console.log(`Gold required: ${Math.ceil(gold)}`);
+  console.log(`Final pyramid height: ${Math.floor(steps*increment)}`);
 }
 
-pyramid(11,1);
-pyramid(11,0.75);
-pyramid(12,1);
-pyramid(23,0.5);
+pyramid(11, 1);
+pyramid(11, 0.75);
+pyramid(12, 1);
+pyramid(23, 0.5);
