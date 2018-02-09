f349e885ddcff39bc10ce7e9462492b425ca441e
tsekotsolov <tsekotsolov@gmail.com>
2018-02-09 21:18:58 +0200

    up

--- 07.Exams/Exam 16.10.2016/P03_SpyMaster/spy.js
+++ 07.Exams/Exam 16.10.2016/P03_SpyMaster/spy.js
@@ -2,7 +2,7 @@ function spy(input = []) {
 
   let specialKey = input[0];
 
-  let pattern = `( |^)(${specialKey}\\s+)([!%$#A-Z]{8,})( |\\.|,|$)`;
+  let pattern = `(\\s|^)(${specialKey}\\s+)([!%$#A-Z]{8,})([\\s.,]|$)`;
   let regex = new RegExp(pattern, 'gi');
 
   for (let i = 1; i < input.length; i++) {
@@ -11,11 +11,9 @@ function spy(input = []) {
     while (match = regex.exec(currentString)) {
       let foundWord = match[3];
 
-      if (foundWord !== foundWord.toUpperCase()) {
-        continue;
+      if (foundWord === foundWord.toUpperCase()) {
+        input[i] = input[i].replace(match[0], match[1] + match[2] + decode(foundWord) + match[4]);
       }
-
-      input[i] = input[i].replace(match[0],match[1] + match[2] +decode(foundWord)+ match[4]);
 
     }
     console.log(input[i]);
@@ -27,48 +25,43 @@ function spy(input = []) {
     for (let char of word) {
       if (char === "!") {
         result += "1"
-      }
-      else if (char === "%") {
+      } else if (char === "%") {
         result += "2"
-      }
-  
-      else if (char === "#") {
+      } else if (char === "#") {
         result += "3"
-      }
-      else if (char === "$") {
+      } else if (char === "$") {
         result += "4"
-      }
-      else {
+      } else {
         result += char;
       }
     }
-    return(result.toLowerCase());
+    return (result.toLowerCase());
   }
 }
 
-    // spy([ 'specialKey',
-    // 'In this text the specialKey HELLOWORLD! is correct, but specialKey HELLOWORLD!',
-    // 'the following specialKey $HelloWorl#d and spEcIaLKEy HOLLOWORLD1 are not, while',
-    // 'SpeCIaLkeY   SOM%%ETH$IN and SPECIALKEY ##$$##$$ are!' ]);
+// spy([ 'specialKey',
+// 'In this text the specialKey HELLOWORLD! is correct, but specialKey HELLOWORLD!',
+// 'the following specialKey $HelloWorl#d and spEcIaLKEy HOLLOWORLD1 are not, while',
+// 'SpeCIaLkeY   SOM%%ETH$IN and SPECIALKEY ##$$##$$ are!' ]);
 
-    // spy([ 'enCode',
-    // 'Some messages are just not encoded what can you do?',
-    // 'RE - ENCODE THEMNOW! - he said. RE - ENCODE THEMNOW! - he said.',
-    // 'Damn encode, ITSALLHETHINKSABOUT, eNcoDe BULL$#!%.' ]
-    // )
+// spy([ 'enCode',
+// 'Some messages are just not encoded what can you do?',
+// 'RE - ENCODE THEMNOW! - he said. RE - ENCODE THEMNOW! - he said.',
+// 'Damn encode, ITSALLHETHINKSABOUT, eNcoDe BULL$#!%.' ]
+// )
 
-    spy(
+spy(
   [
-'tricky',
-`And now the tricky tests`,
-`Tricky CAREFULL!#$%; with what you decode Tricky CAREFULL!#$%`,
-`Tricky HERECOMESDASH- with what you decode Tricky HERECOMESDASH -`,
-`Try again stricky NOTTHEFIRSTONE  tricky NOTTHEFIRSTONE`,
-`Be very carefull now trICkY plainwrong, trICkY PLAINWRONG`,
-`next challenge (tRickY SOME$WORDS) tRickY SOME$WORDS`,
-`It's tricky TOUSETHECORRECTREPLACE? tricky TOUSETHECORRECTREPLACE ,`,
-`now with commas triCky RAND!$OM%$#TE!#XT, triCky RAND!$OM%$#TE!#XT.`,
-`DON'T match this plz TRICKY | TEXT#TEXT. TRICKY  TEXT#TEXT.`,
-`Try with commas -triCkY COMMAHERE, triCkY COMMAHERE, wow`,
+    'tricky',
+    `And now the tricky tests`,
+    `Tricky CAREFULL!#$%; with what you decode Tricky CAREFULL!#$%`,
+    `Tricky HERECOMESDASH- with what you decode Tricky HERECOMESDASH -`,
+    `Try again stricky NOTTHEFIRSTONE  tricky NOTTHEFIRSTONE`,
+    `Be very carefull now trICkY plainwrong, trICkY PLAINWRONG`,
+    `next challenge (tRickY SOME$WORDS) tRickY SOME$WORDS`,
+    `It's tricky TOUSETHECORRECTREPLACE? tricky TOUSETHECORRECTREPLACE ,`,
+    `now with commas triCky RAND!$OM%$#TE!#XT, triCky RAND!$OM%$#TE!#XT.`,
+    `DON'T match this plz TRICKY | TEXT#TEXT. TRICKY  TEXT#TEXT.`,
+    `Try with commas -triCkY COMMAHERE, triCkY COMMAHERE, wow`,
   ]
-    )
+)
